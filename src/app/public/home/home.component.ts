78dcbc2772005c31cad9f52150d34685e5aa1eff
Alex-Pablo <alexpabloanibal@gmail.com>
2024-06-16 23:05:17 -0600

    fixed

--- src/app/public/home/home.component.ts
+++ src/app/public/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 import { NavbarTopComponent } from '../../shared/components/public/navbar-top/navbar-top.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarTopComponent ],
+  imports: [NavbarTopComponent,RouterModule ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
     production: true,
-    baseUrlApi:'https://coreaquavet20240613235655.azurewebsites.net'
+    baseUrlApi: 'https://localhost:7168'
+
+    // baseUrlApi:'https://coreaquavet20240613235655.azurewebsites.net'
 };
