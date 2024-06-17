d4deb04588d93fe08e67a93dcedf3ab4b3d9ca1c
Alex-Pablo <alexpabloanibal@gmail.com>
2024-06-16 23:08:05 -0600

    fixed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,4 @@
 export const environment = {
     production: true,
-    baseUrlApi: 'https://localhost:7168'
-
-    // baseUrlApi:'https://coreaquavet20240613235655.azurewebsites.net'
+    baseUrlApi:'https://coreaquavet20240613235655.azurewebsites.net'
 };
